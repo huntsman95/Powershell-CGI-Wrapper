42c3ee49b287ff46234fb0e7e07f3853d6cba5e4
huntsman95 <huntsman95@gmail.com>
2022-06-11 13:24:53 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
+using System.Web;
+
+string defaultHeader = "Content type: text/html" + Environment.NewLine + Environment.NewLine;
+
+string psargs = HttpUtility.UrlDecode(Environment.GetEnvironmentVariable("QUERY_STRING"));
+psargs = psargs.Replace("\"", "\"\"");
+string[] psargarray = psargs.Split('&');
+for(int i = 0; i < psargarray.Length; i++)
+{
+    psargarray[i] = Regex.Replace(psargarray[i], @"(\w+)=(.*)", "-$1 \"$2\"");
+}
+psargs = String.Join(' ',psargarray);
+
+Process cmd = new Process();
+cmd.StartInfo.FileName = "pwsh.exe";
+cmd.StartInfo.Arguments = "-noprofile -file " + args[0] + " " + psargs;
+cmd.StartInfo.RedirectStandardInput = true;
+cmd.StartInfo.RedirectStandardOutput = true;
+cmd.StartInfo.CreateNoWindow = true;
+cmd.StartInfo.UseShellExecute = false;
+cmd.Start();
+
+string OutputBuffer = "";
+
+while (!cmd.HasExited)
+{
+    OutputBuffer += cmd.StandardOutput.ReadToEnd();
+}
+
+if (!(Regex.Match(OutputBuffer, "Content-type:.*").Success))
+{
+    OutputBuffer = defaultHeader + OutputBuffer;
+}
+
+//cmd.WaitForExit();
+Console.WriteLine(OutputBuffer);
